c15f0c43d4dacc939f81dc58b7e8ad43303944db
1 <1@DESKTOP-MU1VK5T>
2023-08-01 01:03:57 +0500

    Добавьте файлы проекта.

---
+++ EmployeesAppenderPlugin/Plugin.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections.Generic;
+using System.IO;
+using PhoneApp.Domain.Attributes;
+using PhoneApp.Domain.DTO;
+using PhoneApp.Domain.Interfaces;
+using Newtonsoft.Json;
+using NLog;
+using System.Linq;
+
+namespace EmployeesAppenderPlugin
+{
+    [Author(Name = "Rageon")]
+    public class Plugin : IPluggable
+    {
+        public IEnumerable<DataTransferObject> Run(IEnumerable<DataTransferObject> args)
+        {
+            string jsonStringRead = File.ReadAllText("directory.json");
+            List<EmployeesDTO> source = JsonConvert.DeserializeObject<List<EmployeesDTO>>(jsonStringRead);
+            Plugin.logger.Info(string.Format("Loaded {0} employees from directory", source.Count<EmployeesDTO>()));
+            return source.Cast<DataTransferObject>();
+        }
+
+        private static readonly Logger logger = LogManager.GetCurrentClassLogger();
+    }
+}
